4968bbec7dfab2e68ea6d6bcf73d94055d0af569
fanxing <871481270@qq.com>
2018-04-23 20:35:20 +0800

    集成swargger和mysql jpa

--- src/main/java/com/fanxingzhiduoshao/user/authentication/center/AuthenticationCenterApplication.java
+++ src/main/java/com/fanxingzhiduoshao/user/authentication/center/AuthenticationCenterApplication.java
@@ -3,8 +3,10 @@ package com.fanxingzhiduoshao.user.authentication.center;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class AuthenticationCenterApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/fanxingzhiduoshao/user/authentication/center/controller/UserCenterController.java
+++ src/main/java/com/fanxingzhiduoshao/user/authentication/center/controller/UserCenterController.java
@@ -1,17 +1,22 @@
 package com.fanxingzhiduoshao.user.authentication.center.controller;
 
 
+import com.fanxingzhiduoshao.user.authentication.center.controller.service.UserService;
 import com.fanxingzhiduoshao.user.authentication.center.controller.vo.AjaxResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller()
+@Api
 @RequestMapping("/user")
 public class UserCenterController {
+
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/index")
     public ModelAndView index() {
@@ -21,12 +26,23 @@ public class UserCenterController {
     }
 
     @GetMapping(name = "", produces = "application/json;charset=utf-8")
-    public AjaxResult addUser() {
+    @ResponseBody
+    @ApiOperation("获取用户列表")
+    public AjaxResult getUser() {
+
 
         return AjaxResult.SUCCESSS;
     }
 
     @PostMapping(name = "", produces = "application/json;charset=utf-8")
+    @ResponseBody
+    public AjaxResult addUser() {
+        userService.addUser();
+        return AjaxResult.SUCCESSS;
+    }
+
+    @PutMapping(name = "", produces = "application/json;charset=utf-8")
+    @ResponseBody
     public AjaxResult updateUser() {
 
         return AjaxResult.SUCCESSS;
